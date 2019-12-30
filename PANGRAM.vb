aad4664c6e699e3b6d54a1ac5c306ad6fc677a4e
Nabi Bux <55255396+NabiBux9565@users.noreply.github.com>
2019-12-30 19:22:07 +0500

    PANGRAM

---
+++ PANGRAM.vb
@@ -0,0 +1,49 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Str2 As String
+        Dim Count As Integer
+        Dim NextChar As Char
+        Dim IsPangram As Boolean
+
+
+
+        Str1 = ""
+        Str2 = ""
+        Count = 0
+        NextChar = ""
+        IsPangram = True
+
+
+
+        Console.WriteLine("                     PANGRAM ")
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
+        Str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+        Console.WriteLine("Enter the string : ")
+        Str2 = Console.ReadLine
+        Str2 = UCase(Str2)
+
+        For Count = 1 To Len(Str1)
+            NextChar = Mid(Str1, Count, 1)
+            If InStr(Str1, NextChar) = 0 Then
+                IsPangram = False
+                Exit For
+            End If
+        Next
+
+        If IsPangram = True Then
+            Console.WriteLine("It is a pangram ")
+        ElseIf IsPangram = False Then
+            Console.WriteLine("Its not pangram ")
+        End If
+
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
